5987ec13bd8ac1ce2a15e699bf5bfd9ef4670dee
ChuNgocBinh96 <ChuNgocBinh96>
2022-01-16 02:53:19 +0700

    fix llisst post

--- social-app/src/Components/ListPosts/ListPosts.js
+++ social-app/src/Components/ListPosts/ListPosts.js
@@ -4,47 +4,9 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import request from "../../Api/request";
 import LoadingPost from '../Loading/LoadingPost'
 
-export default function ListPosts() {
-  const [posts, setPosts] = React.useState([])
-  const [skip, setSkip] = React.useState(0)
-  const [load, setLoad] = React.useState(false)
-
-  const fetchPosts = async (skip) => {
-    setLoad(true)
-    const res = await request({
-      url: '/posts',
-      params: {
-        skip: skip
-      },
-      method: 'GET',
-    })
-    if (res.data) {
-      setLoad(false)
-      return res.data
-    }
-  }
-
-  const renderPosts = async ()=> {
-    const data = await fetchPosts(skip)
-    setPosts(data)
-  }
-
-  React.useEffect(() => {
-    renderPosts()
-  }, [])
-
-  const scrollData = async () => {
-    setLoad(true)
-    const newSkip = (skip +4)
-    const data =  await fetchPosts(skip)
-    setPosts(prePosts=> [...prePosts, ...data])
-    setSkip(newSkip)
-    setLoad(false)
-  }
-
+export default function ListPosts({posts, load, scrollData}) {
 
   return (
-
     <InfiniteScroll
       dataLength={posts.length} //This is important field to render the next data
       next={scrollData}
