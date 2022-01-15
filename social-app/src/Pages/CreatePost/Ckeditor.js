6d59ae775274d8250df7c04656536393cacd6b29
ChuNgocBinh96 <ChuNgocBinh96>
2022-01-15 16:20:03 +0700

    remove package.js

--- social-app/src/Pages/CreatePost/Ckeditor.js
+++ social-app/src/Pages/CreatePost/Ckeditor.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
-export default function Ckeditor({ handleBlur, text }) {
+export default function Ckeditor({ value, setValue }) {
   return (
     <div className="mt-2">
       <h4>Content</h4>
-      <CKEditor
-        editor={ClassicEditor}
-        data={text}
-        onBlur={(event, editor) => {
-          const data = editor.getData();
-          handleBlur(data)
-        }}
-      />
+       <ReactQuill theme="snow" value={value} onChange={setValue}/>
     </div>
   );
 }
