9940b247edd26a937e10160627c3f2253b5e28b9
Hung Duong <duonghung.tn1509@gmail.com>
2022-01-21 18:03:15 +0700

    update

--- social-app/src/Components/ListMenuBar/MenuItem.js
+++ social-app/src/Components/ListMenuBar/MenuItem.js
@@ -1,45 +1,46 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function MenuItem() {
-
   const links = [
     {
-      src: 'https://www.facebook.com/rsrc.php/v3/yx/r/-XF4FQcre_i.png',
-      value: 'Friends'
+      src: "https://www.facebook.com/rsrc.php/v3/yx/r/-XF4FQcre_i.png",
+      value: "Friends",
+      link: "/friends",
     },
     {
-      src: 'https://www.facebook.com/rsrc.php/v3/yr/r/2uPlV4oORjU.png',
-      value: 'Saved'
+      src: "https://static.xx.fbcdn.net/rsrc.php/v3/yq/r/YF1bztyGuX-.png",
+      value: "Messenger",
+      link: "/message",
     },
-    {
-      src: 'https://www.facebook.com/rsrc.php/v3/yD/r/mk4dH3FK0jT.png',
-      value: 'Groups'
-    },
-    {
-      src: 'https://static.xx.fbcdn.net/rsrc.php/v3/yq/r/YF1bztyGuX-.png',
-      value: 'Messenger'
-    },
-  ]
+  ];
   return (
-    <div className='mb-2 d-flex justify-content-start flex-column' > 
-      <ul className='list-group' >
+    <div className="mb-2 d-flex justify-content-start flex-column">
+      <ul className="list-group">
         {links.map((link, index) => {
           return (
-            <li key={index} className='list-group-item border-0' style={{ backgroundColor: '#F0F2F5' }}>
-              <a>
-                <div className='d-flex align-items-center'>
-                  <div className='me-3'>
-                    <img src={link.src} style={{ width: '36px', height: '36px' }} />
+            <li
+              key={index}
+              className="list-group-item border-0"
+              style={{ backgroundColor: "#F0F2F5",  }}
+            >
+              <Link to={link.link}>
+                <div className="d-flex align-items-center">
+                  <div className="me-3">
+                    <img
+                      src={link.src}
+                      style={{ width: "36px", height: "36px" }}
+                    />
                   </div>
                   <div>
                     <b>{link.value}</b>
                   </div>
                 </div>
-              </a>
+              </Link>
             </li>
-          )
+          );
         })}
       </ul>
     </div>
-  )
+  );
 }
